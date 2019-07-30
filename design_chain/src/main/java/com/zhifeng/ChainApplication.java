d5bde12f14e038854b497bb2e698e3e0d20afa7a
ganzhifeng <ganzhifeng@viewshare.cn>
2019-07-30 20:57:16 +0800

    项目初始化搭建，未集成mysql

--- design_chain/src/main/java/com/zhifeng/ChainApplication.java
+++ design_chain/src/main/java/com/zhifeng/ChainApplication.java
@@ -3,6 +3,12 @@ package com.zhifeng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 责任链模式项目启动类
+ * @Author ganzhifeng
+ * @Description
+ * @Date 2019/7/30 0030
+ */
 @SpringBootApplication
 public class ChainApplication {
 
